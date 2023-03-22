7821b312ca19b089369f8416668cd5462479ff17
Alexander Kharichev <myx@myx.ru>
2023-03-23 00:41:46 +0200

    * java11: Use String.isBlank()

--- java/ru/myx/ae2virtual/FactoryVirtualSource.java
+++ java/ru/myx/ae2virtual/FactoryVirtualSource.java
@@ -12,31 +12,28 @@ import ru.myx.ae3.produce.ObjectFactory;
 import ru.myx.ae3.produce.Produce;
 import ru.myx.ae3.serve.ServeRequest;
 
-/**
- * @author myx
- *
- */
+/** @author myx */
 public final class FactoryVirtualSource implements ObjectFactory<Object, ServeRequest> {
-	
+
 	private static final Class<?>[] TARGETS = {
 			ServeRequest.class
 	};
-
+	
 	private static final Class<?>[] SOURCES = null;
-
+	
 	private static final String[] VARIETY = {
 			"VIRTUAL"
 	};
-
+	
 	@Override
 	public final boolean accepts(final String variant, final BaseObject attributes, final Class<?> source) {
-		
-		return Base.getString(attributes, "id", "").trim().length() > 0;
-	}
 
+		return !Base.getString(attributes, "id", "").isBlank();
+	}
+	
 	@Override
 	public final ObjectSource<ServeRequest> prepare(final String type, final BaseObject attributes, final Object context) {
-		
+
 		final String id = Base.getString(attributes, "id", null);
 		try {
 			final PipeVirtual virtual = Pack.VIRTUALS.get(id);
@@ -59,10 +56,10 @@ public final class FactoryVirtualSource implements ObjectFactory<Object, ServeRe
 			return created;
 		}
 	}
-
+	
 	@Override
 	public final ServeRequest produce(final String variant, final BaseObject attributes, final Object source) {
-		
+
 		final ObjectSource<ServeRequest> channel = this.prepare(variant, attributes, source);
 		while (!channel.isReady() && !channel.isExhausted()) {
 			try {
@@ -77,22 +74,22 @@ public final class FactoryVirtualSource implements ObjectFactory<Object, ServeRe
 			? null
 			: channel.next();
 	}
-
+	
 	@Override
 	public final Class<?>[] sources() {
-		
+
 		return FactoryVirtualSource.SOURCES;
 	}
-
+	
 	@Override
 	public final Class<?>[] targets() {
-		
+
 		return FactoryVirtualSource.TARGETS;
 	}
-
+	
 	@Override
 	public final String[] variety() {
-		
+
 		return FactoryVirtualSource.VARIETY;
 	}
 }
